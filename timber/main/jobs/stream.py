9d16dbbed2fcafecccfc6e191464ff1f641b0d26
AinL <gmlwns5176@gmail.com>
2024-02-26 22:14:43 +0900

    update

--- timber/main/jobs/stream.py
+++ timber/main/jobs/stream.py
@@ -59,13 +59,13 @@ def job_stream(args, model, tokenizer, device):
             if isinstance(model, LLM):
                 prompts = [input_text, ] * args.batch_size
                 sampling_params = SamplingParams(
-                    temperature=0.7, 
-                    top_p=0.9,
+                    temperature=0.7,
+                    top_p=0.3,
                     top_k=500,
                     max_tokens=512,
                     # max_tokens=16,
-                    frequency_penalty=0.0,
-                    repetition_penalty=1.1,
+                    frequency_penalty=0.2,
+                    repetition_penalty=1.0,
                     ignore_eos=True,
                     skip_special_tokens=False,
                     # max_tokens=inputs.input_ids.shape[-1] + 32,
