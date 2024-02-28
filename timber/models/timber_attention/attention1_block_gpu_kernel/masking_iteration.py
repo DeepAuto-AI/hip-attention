953163eb56bad8fbd5096017434a565d8edb347d
AinL <gmlwns5176@gmail.com>
2024-02-29 00:24:39 +0900

    reduce stages

--- timber/models/timber_attention/attention1_block_gpu_kernel/masking_iteration.py
+++ timber/models/timber_attention/attention1_block_gpu_kernel/masking_iteration.py
@@ -1061,6 +1061,6 @@ def masking_iteration(
         
         # num_warps=max(2, (min(8, max(BLOCK_TMASK_K//32, 1)) if SPARQ else 4) // GRID_KSTRIDE),
         num_warps=4,
-        num_stages=8,
+        num_stages=2,
         enable_warp_specialization=False,
     )
