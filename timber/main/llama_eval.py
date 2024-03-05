b85d8aa8760432c9b5be449f045f88fa5822856a
AinL <gmlwns5176@gmail.com>
2024-03-05 16:05:42 +0900

    update

--- timber/main/llama_eval.py
+++ timber/main/llama_eval.py
@@ -26,6 +26,7 @@ def load_vllm_model(args: ArgsType):
     MODELS = {
         'vllm_llama32k': 'togethercomputer/LLaMA-2-7B-32K',
         'vllm_llama128k': 'NousResearch/Yarn-Llama-2-7b-128k',
+        'vllm_llama13b_128k': 'NousResearch/Yarn-Llama-2-13b-128k',
         'vllm_llama100k': 'Yukang/Llama-2-7b-longlora-100k-ft',
         'vllm_llama32k_instruct': 'togethercomputer/Llama-2-7B-32K-Instruct',
         'vllm_llama1b': 'princeton-nlp/Sheared-LLaMA-1.3B',
