5e1ef8da4d331f6c37c648f468400c93ccdb4890
Daniel Geon Park <mujjingun@gmail.com>
2024-03-12 02:52:23 +0900

    implement variable attend lengths

--- timber/models/attn_l1_loss.py
+++ timber/models/attn_l1_loss.py
@@ -16,6 +16,7 @@ def compute_attn_lp_loss_kernel(
         HDIM_MAX: tl.constexpr,
         KV_BLOCK_SIZE: tl.constexpr, Q_BLOCK_SIZE: tl.constexpr,
         IS_CAUSAL: tl.constexpr,
+        attend_lengths, attend_lengths_stride_n, attend_lengths_stride_t,
         l, l_stride_n, l_stride_h, l_stride_t,
         m, m_stride_n, m_stride_h, m_stride_t,
         output, output_stride_n, output_stride_h, output_stride_t,
@@ -27,6 +28,30 @@ def compute_attn_lp_loss_kernel(
     q_idx = tl.arange(0, Q_BLOCK_SIZE)
     kv_idx = tl.arange(0, KV_BLOCK_SIZE)
     d_idx = tl.arange(0, HDIM_MAX)
+
+    q_chunk = tl.load(
+        q +
+        n_idx * q_stride_n +
+        h_idx * q_stride_h +
+        (q_begin + q_idx)[:, None] * q_stride_t +
+        d_idx[None, :] * q_stride_hdim,
+        mask=(
+            (q_begin + q_idx < TDST)[:, None] &
+            (d_idx < HDIM)[None, :]
+        ),
+        other=0
+    )  # [q_blk, hd]
+
+    attend_lengths_chunk = None
+    if attend_lengths is not None:
+        attend_lengths_chunk = tl.load(
+            attend_lengths +
+            n_idx * attend_lengths_stride_n +
+            (q_begin + q_idx) * attend_lengths_stride_t,
+            mask=(q_begin + q_idx < TDST),
+            other=0
+        )  # [q_blk]
+
     for kv_begin in range(0, TSRC, KV_BLOCK_SIZE):
         k_chunk = tl.load(
             k +
@@ -40,18 +65,6 @@ def compute_attn_lp_loss_kernel(
             ),
             other=0
         )  # [hd, kv_blk]
-        q_chunk = tl.load(
-            q +
-            n_idx * q_stride_n +
-            h_idx * q_stride_h +
-            (q_begin + q_idx)[:, None] * q_stride_t +
-            d_idx[None, :] * q_stride_hdim,
-            mask=(
-                (q_begin + q_idx < TDST)[:, None] &
-                (d_idx < HDIM)[None, :]
-            ),
-            other=0
-        )  # [q_blk, hd]
         output_chunk = tl.load(
             output +
             n_idx * output_stride_n +
@@ -86,6 +99,12 @@ def compute_attn_lp_loss_kernel(
         if IS_CAUSAL:
             attn_scores = tl.where(
                 (kv_begin + kv_idx)[None, :] > (q_begin + q_idx)[:, None],
+                -1e9,
+                attn_scores
+            )
+        if attend_lengths is not None:
+            attn_scores = tl.where(
+                (kv_begin + kv_idx)[None, :] >= attend_lengths_chunk[:, None],
                 -1e9,
                 attn_scores
             )
@@ -139,6 +158,7 @@ def compute_attn_lp_loss_kernel_backward(
         HDIM_MAX: tl.constexpr,
         KV_BLOCK_SIZE: tl.constexpr, Q_BLOCK_SIZE: tl.constexpr,
         IS_CAUSAL: tl.constexpr,
+        attend_lengths, attend_lengths_stride_n, attend_lengths_stride_t,
         l, l_stride_n, l_stride_h, l_stride_t,
         m, m_stride_n, m_stride_h, m_stride_t,
         grad_q, grad_q_stride_n, grad_q_stride_h, grad_q_stride_t, grad_q_stride_hdim,
@@ -151,6 +171,70 @@ def compute_attn_lp_loss_kernel_backward(
     q_idx = tl.arange(0, Q_BLOCK_SIZE)
     kv_idx = tl.arange(0, KV_BLOCK_SIZE)
     d_idx = tl.arange(0, HDIM_MAX)
+
+    q_chunk = tl.load(
+        q +
+        n_idx * q_stride_n +
+        h_idx * q_stride_h +
+        (q_begin + q_idx)[None, :] * q_stride_t +
+        d_idx[:, None] * q_stride_hdim,
+        mask=(
+                (q_begin + q_idx < TDST)[None, :] &
+                (d_idx < HDIM)[:, None]
+        ),
+        other=0
+    )  # [hd, q_blk]
+    output_chunk = tl.load(
+        output +
+        n_idx * output_stride_n +
+        h_idx * output_stride_h +
+        (q_begin + q_idx)[None, :] * output_stride_t,
+        mask=(
+            (q_begin + q_idx < TDST)[None, :]
+        ),
+        other=0
+    )  # [1, q_blk]
+    grad_output_chunk = tl.load(
+        grad_output +
+        n_idx * grad_output_stride_n +
+        h_idx * grad_output_stride_h +
+        (q_begin + q_idx)[None, :] * grad_output_stride_t,
+        mask=(
+            (q_begin + q_idx < TDST)[None, :]
+        ),
+        other=0
+    )  # [1, q_blk]
+    l_chunk = tl.load(
+        l +
+        n_idx * l_stride_n +
+        h_idx * l_stride_h +
+        (q_begin + q_idx)[None, :] * l_stride_t,
+        mask=(
+            (q_begin + q_idx < TDST)[None, :]
+        ),
+        other=0
+    )  # [1, q_blk]
+    m_chunk = tl.load(
+        m +
+        n_idx * m_stride_n +
+        h_idx * m_stride_h +
+        (q_begin + q_idx)[None, :] * m_stride_t,
+        mask=(
+            (q_begin + q_idx < TDST)[None, :]
+        ),
+        other=-1e9
+    )  # [1, q_blk]
+
+    attend_lengths_chunk = None
+    if attend_lengths is not None:
+        attend_lengths_chunk = tl.load(
+            attend_lengths +
+            n_idx * attend_lengths_stride_n +
+            (q_begin + q_idx) * attend_lengths_stride_t,
+            mask=(q_begin + q_idx < TDST),
+            other=0
+        )
+
     for kv_begin in range(0, TSRC, KV_BLOCK_SIZE):
         k_chunk = tl.load(
             k +
@@ -164,58 +248,6 @@ def compute_attn_lp_loss_kernel_backward(
             ),
             other=0
         )  # [kv_blk, hd]
-        q_chunk = tl.load(
-            q +
-            n_idx * q_stride_n +
-            h_idx * q_stride_h +
-            (q_begin + q_idx)[None, :] * q_stride_t +
-            d_idx[:, None] * q_stride_hdim,
-            mask=(
-                (q_begin + q_idx < TDST)[None, :] &
-                (d_idx < HDIM)[:, None]
-            ),
-            other=0
-        )  # [hd, q_blk]
-        output_chunk = tl.load(
-            output +
-            n_idx * output_stride_n +
-            h_idx * output_stride_h +
-            (q_begin + q_idx)[None, :] * output_stride_t,
-            mask=(
-                (q_begin + q_idx < TDST)[None, :]
-            ),
-            other=0
-        )  # [1, q_blk]
-        grad_output_chunk = tl.load(
-            grad_output +
-            n_idx * grad_output_stride_n +
-            h_idx * grad_output_stride_h +
-            (q_begin + q_idx)[None, :] * grad_output_stride_t,
-            mask=(
-                (q_begin + q_idx < TDST)[None, :]
-            ),
-            other=0
-        )  # [1, q_blk]
-        l_chunk = tl.load(
-            l +
-            n_idx * l_stride_n +
-            h_idx * l_stride_h +
-            (q_begin + q_idx)[None, :] * l_stride_t,
-            mask=(
-                (q_begin + q_idx < TDST)[None, :]
-            ),
-            other=0
-        )  # [1, q_blk]
-        m_chunk = tl.load(
-            m +
-            n_idx * m_stride_n +
-            h_idx * m_stride_h +
-            (q_begin + q_idx)[None, :] * m_stride_t,
-            mask=(
-                (q_begin + q_idx < TDST)[None, :]
-            ),
-            other=-1e9
-        )  # [1, q_blk]
 
         attn_scores = tl.dot(k_chunk, q_chunk).to(tl.float32)  # [kv_blk, q_blk]
         logP = attn_scores - m_chunk - tl.log(l_chunk)  # [kv_blk, q_blk]
@@ -226,6 +258,12 @@ def compute_attn_lp_loss_kernel_backward(
         if IS_CAUSAL:
             grad_S = tl.where(
                 (kv_begin + kv_idx)[:, None] > (q_begin + q_idx)[None, :],
+                0.0,
+                grad_S
+            )
+        if attend_lengths is not None:
+            grad_S = tl.where(
+                (kv_begin + kv_idx)[:, None] >= attend_lengths_chunk[None, :],
                 0.0,
                 grad_S
             )
@@ -262,10 +300,11 @@ class AttnLpLoss(torch.autograd.Function):
 
     @staticmethod
     def forward(ctx,  # noqa
-                q, k, N, H, TDST, TSRC, HDIM, p, is_causal,
+                q, k, N, H, TDST, TSRC, HDIM, p, is_causal, attend_lengths,
                 KV_BLOCK_SIZE, Q_BLOCK_SIZE):
         assert q.ndim == 4
         assert k.ndim == 4
+        assert attend_lengths.ndim == 2 if attend_lengths is not None else True
         l = torch.full((N, H, TDST), 0.0, device=q.device)  # [bsz, num_heads, q_len]
         m = torch.full((N, H, TDST), -1e9, device=q.device)  # [bsz, num_heads, q_len]
         result = torch.zeros((N, H, TDST), device=q.device)
@@ -280,13 +319,18 @@ class AttnLpLoss(torch.autograd.Function):
             triton.next_power_of_2(HDIM),
             KV_BLOCK_SIZE, Q_BLOCK_SIZE,
             is_causal,
+            attend_lengths, *(attend_lengths.stride() if attend_lengths is not None else (None, None)),
             l, *l.stride(),
             m, *m.stride(),
             result, *result.stride(),
         )
         torch.cuda.set_device(orig_device)
 
-        ctx.save_for_backward(q, k, l, m, result)
+        if attend_lengths is not None:
+            ctx.save_for_backward(q, k, l, m, result, attend_lengths)
+        else:
+            ctx.save_for_backward(q, k, l, m, result)
+        ctx.has_attend_lengths = attend_lengths is not None
         ctx.N, ctx.H, ctx.TDST, ctx.TSRC, ctx.HDIM = N, H, TDST, TSRC, HDIM
         ctx.p, ctx.is_causal = p, is_causal
         ctx.KV_BLOCK_SIZE, ctx.Q_BLOCK_SIZE = KV_BLOCK_SIZE, Q_BLOCK_SIZE
@@ -296,7 +340,11 @@ class AttnLpLoss(torch.autograd.Function):
 
     @staticmethod
     def backward(ctx, grad_output: torch.Tensor):  # noqa
-        q, k, l, m, result = ctx.saved_tensors
+        if ctx.has_attend_lengths:
+            q, k, l, m, result, attend_lengths = ctx.saved_tensors
+        else:
+            q, k, l, m, result = ctx.saved_tensors
+            attend_lengths = None
         N, H, TDST, TSRC, HDIM = ctx.N, ctx.H, ctx.TDST, ctx.TSRC, ctx.HDIM
         p, is_causal = ctx.p, ctx.is_causal
         KV_BLOCK_SIZE, Q_BLOCK_SIZE = ctx.KV_BLOCK_SIZE, ctx.Q_BLOCK_SIZE
@@ -318,6 +366,7 @@ class AttnLpLoss(torch.autograd.Function):
             triton.next_power_of_2(HDIM),
             KV_BLOCK_SIZE, Q_BLOCK_SIZE,
             is_causal,
+            attend_lengths, *(attend_lengths.stride() if attend_lengths is not None else (None, None)),
             l, *l.stride(),
             m, *m.stride(),
             grad_q, *grad_q.stride(),
@@ -327,16 +376,17 @@ class AttnLpLoss(torch.autograd.Function):
 
         return (
             grad_q, grad_k,
-            None, None, None, None, None, None, None,
+            None, None, None, None, None, None, None, None,
             None, None
         )
 
 
-def compute_attn_lp_loss_triton(q, k, p, is_causal=True, do_average=True,
+def compute_attn_lp_loss_triton(q, k, p, is_causal=True, attend_lengths=None, do_average=True,
                                 KV_BLOCK_SIZE=64, Q_BLOCK_SIZE=64):
     assert q.ndim == 4 and k.ndim == 4
     N, H, TDST, TSRC, HDIM = q.shape[0], q.shape[1], q.shape[2], k.shape[2], q.shape[3]
-    result = AttnLpLoss.apply(q, k, N, H, TDST, TSRC, HDIM, p, is_causal, KV_BLOCK_SIZE, Q_BLOCK_SIZE)
+    result = AttnLpLoss.apply(
+        q, k, N, H, TDST, TSRC, HDIM, p, is_causal, attend_lengths, KV_BLOCK_SIZE, Q_BLOCK_SIZE)
     if do_average:
         result = result.mean(dim=-1)  # [bsz, num_heads]
     return result
@@ -406,12 +456,17 @@ def compute_attn_lp_loss(q, k, p, is_causal=True, do_average=True,
     return attn_sparsity_loss
 
 
-def compute_attn_lp_loss_orig(q, k, p, is_causal=True, do_average=True):
+def compute_attn_lp_loss_orig(q, k, p, is_causal=True, attend_lengths=None, do_average=True):
     assert q.ndim == 4 and k.ndim == 4
     attn_scores = torch.einsum("bhqd,bhkd->bhqk", q, k)  # shape [bsz, num_heads, q_len, kv_len]
+    assert not (is_causal and attend_lengths is not None)
     if is_causal:
         invalid_mask = torch.triu(torch.ones(attn_scores.shape[-2:], device=q.device, dtype=torch.bool), 1)
         attn_scores = attn_scores.masked_fill(invalid_mask, -1e9)
+    if attend_lengths is not None:  # shape [bsz, q_len]
+        attn_scores[
+            (torch.arange(k.shape[-2], device=q.device) >= attend_lengths[:, None, :, None]).expand_as(attn_scores)
+        ] = -1e9
     attn_log_probs = torch.nn.functional.log_softmax(attn_scores, dim=-1)
     attn_sparsity_loss = torch.exp(attn_log_probs * p).sum(dim=-1)
     attn_sparsity_loss = attn_sparsity_loss**(1/p)
@@ -424,7 +479,7 @@ def test_correctness():
     #torch.autograd.set_detect_anomaly(True)
     N = 1
     H = 40
-    TDST, TSRC = 64, 64
+    TDST, TSRC = 1024, 1024
     HDIM = 64
     KV_BLOCK_SIZE, Q_BLOCK_SIZE = 64, 64
     p = 0.5
@@ -434,6 +489,10 @@ def test_correctness():
     do_compare = True
     do_average = False
     is_causal = False
+    use_attend_lengths = False
+    attend_lengths = None
+    if use_attend_lengths:
+        attend_lengths = torch.randperm(TDST, device='cuda').expand(N, TDST)
     noise = torch.randn(N, H, device='cuda') if do_average else torch.randn(N, H, TDST, device='cuda')
 
     for _ in range(3):
@@ -442,10 +501,11 @@ def test_correctness():
         if do_compare:
             torch.cuda.reset_peak_memory_stats()
             time_begin = time.time()
-            l1_loss_orig = compute_attn_lp_loss(
+            l1_loss_orig = compute_attn_lp_loss_orig(
                 q, k, p, is_causal=is_causal, do_average=do_average,
-                KV_BLOCK_SIZE=KV_BLOCK_SIZE,
-                Q_BLOCK_SIZE=Q_BLOCK_SIZE)
+                attend_lengths=attend_lengths)
+                #KV_BLOCK_SIZE=KV_BLOCK_SIZE,
+                #Q_BLOCK_SIZE=Q_BLOCK_SIZE)
             if do_backward:
                 (l1_loss_orig * noise).sum().backward()
                 grad_q_orig = q.grad.detach()
@@ -461,6 +521,7 @@ def test_correctness():
         time_begin = time.time()
         l1_loss = compute_attn_lp_loss_triton(
             q, k, p, is_causal=is_causal, do_average=do_average,
+            attend_lengths=attend_lengths,
             KV_BLOCK_SIZE=KV_BLOCK_SIZE,
             Q_BLOCK_SIZE=Q_BLOCK_SIZE
         )
