ea0d92798d087a6e7721594383eef4bab9d19885
AinL <gmlwns5176@gmail.com>
2024-03-29 10:20:50 +0000

    update bench

--- scripts/bench_stream_1.sh
+++ scripts/bench_stream_1.sh
@@ -8,7 +8,8 @@ export PROMPT_ATTENTION_BACKEND='hip'
 export PAGED_ATTENTION_BACKEND='hip' 
 export HIP_DENSE_LAYERS=4 
 export HIP_K=512
-export CUDA_VISIBLE_DEVICES=0,1
+# export CUDA_VISIBLE_DEVICES=0,1
 export HF_HOME=/d1/heejun/cache/hf_home
 
-/home/ainl/miniconda3/envs/hip/bin/python -b timber/main/llama_eval.py --model vllm_qwen14b_gptq --job stream --batch_size 1 --input samples/16k.md --stride 32000 --max_tokens 3
+echo samples/$FILENAME.md
+/home/ainl/miniconda3/envs/hip/bin/python -b timber/main/llama_eval.py --model vllm_qwen14b_gptq --job stream --batch_size 1 --input samples/$FILENAME.md --stride 32000 --max_tokens 3
