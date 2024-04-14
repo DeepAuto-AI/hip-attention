0fb044e24af115d4a3e8acb094321da33556fb52
AinL <gmlwns5176@gmail.com>
2024-04-14 11:41:47 +0000

    add rader

--- timber/main/scripts/rader_report.py
+++ timber/main/scripts/rader_report.py
@@ -13,6 +13,26 @@ DATA_LONGBENCH = {
     'ymin': 50.0,
 }
 
+DATA_MMLU = {
+    'row_headers': [
+        'LLaMA2 7B', 
+        'HiP k512 PP', 
+        'HiP k1024 PP',
+        'StreamingLLM k512', 
+        'StreamingLLM k1024', 
+    ],
+    'col_headers': ['Humanities', 'STEM', 'Social Sciences', 'Other', 'Average'],
+    'data': [
+        [47.24, 34.81, 48.51, 44.59, 42.76],
+        [45.99, 34.61, 46.85, 44.87, 42.12],
+        [47.25, 35.03, 48.55, 45.61, 43.08],
+        [45.74, 35.17, 45.76, 42.16, 41.40],
+        [47.00, 34.37, 48.09, 44.61, 42.47],
+    ],
+    'theta_offset': 1.0,
+    'ymin': 90.0,
+}
+
 DATA_LMEVAL = {
     # 'row_headers': ['LLaMA2 7B', 'StreamingLLM k128', 'StreamingLLM k256', 'StreamingLLM k512', 'HiP k128 PP', 'HiP k256 PP', 'HiP k512 PP'],
     'row_headers': ['LLaMA2 7B', 'StreamingLLM k256', 'StreamingLLM k512', 'HiP k256 PP', 'HiP k512 PP'],
@@ -31,16 +51,31 @@ DATA_LMEVAL = {
 }
 
 DATA_LMMSEVAL = {
-    'row_headers': ['LLaVA2 13B', 'StremaingLLM k128', 'StremaingLLM k256', 'StremaingLLM k512', 'HiP k128 PP', 'HiP k256 PP', 'HiP k512 PP'],
-    'col_headers': [''],
-    'data': []
+    'row_headers': [
+        'LLaVAv1.6 13B',
+        # 'StremaingLLM k256', 
+        # 'StremaingLLM k512', 
+        # 'StremaingLLM k1024', 
+        'HiP k256 PP',
+        'HiP k512 PP',
+        'HiP k1024 PP',
+    ],
+    'col_headers': ['MME cog.', 'MME percep.', 'MMMU', 'DocVQA', 'Average'],
+    'data': [
+        [324.29, 1575.21, 0.359, 0.7738, 100.0],
+        [312.14, 1468.88, 0.347, 0.6233, 91.68],
+        [307.14, 1515.89, 0.353, 0.7104, 95.27],
+        [337.86, 1553.84, 0.361, 0.7525, 100.16],
+    ],
+    'theta_offset': 0.0,
+    'ymin': 70.0,
 }
 
 import numpy as np
 import matplotlib.pyplot as plt
 from timber.utils import setup_seaborn
 
-setup_seaborn()
+setup_seaborn(legend_fontsize=6)
 
 def loop_list(lst):
     return [*lst, lst[0]]
@@ -73,10 +108,12 @@ def render_rader(chart, name, title, root='saves/raders/'):
     ax.legend(loc='lower center', bbox_to_anchor=(0.5, -0.3), ncols=2)
     ax.set_ylim([chart['ymin'], np.max(data) * 1.0])
     
-    plt.savefig(os.path.join(root, name + '.png'), dpi=200, bbox_inches="tight", pad_inches=0)
-    plt.savefig(os.path.join(root, name + '.pdf'), dpi=200, bbox_inches="tight", pad_inches=0)
+    plt.savefig(os.path.join(root, name + '.png'), dpi=200, bbox_inches="tight", pad_inches=0.01)
+    plt.savefig(os.path.join(root, name + '.pdf'), dpi=200, bbox_inches="tight", pad_inches=0.01)
     print('saved', os.path.join(root, name + '.png'))
 
 if __name__ == '__main__':
+    render_rader(DATA_MMLU, 'plot_rader_mmlu', 'MMLU')
     render_rader(DATA_LONGBENCH, 'plot_rader_longbench', 'LongBench')
     render_rader(DATA_LMEVAL, 'plot_rader_lmeval', 'LM-Eval')
+    render_rader(DATA_LMMSEVAL, 'plot_rader_lmmseval', 'LMMs-Eval')
