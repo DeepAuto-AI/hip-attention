0f9c197f879bcd40785ef4f7c5ad7f29ef8b9adb
AinL <gmlwns5176@gmail.com>
2024-05-14 13:17:17 +0000

    update rader plot

--- timber/main/scripts/rader_report.py
+++ timber/main/scripts/rader_report.py
@@ -3,12 +3,12 @@ import os
 DATA_LONGBENCH = {
     'row_headers': [
         'Qwen1.5 7B', 
-        'StreamingLLM k512', 
-        'StreamingLLM k512', 
-        'HiP k512 PP', 
-        'HiP k1024 PP'
+        'S.LLM k512', 
+        'S.LLM k1024', 
+        'HiP k512', 
+        'HiP k1024'
     ],
-    'col_headers': ['N.QA', 'Qasper', 'H.QA', '2W.MQA', 'G.R.', 'M.N.', 'Avg.'],
+    'col_headers': ['NarrativeQA', 'Qasper', 'HotpotQA', '2WikiMQA', 'GovReport', 'MultiNews', 'Average'],
     'data': [
         [20.94, 40.25, 47.92, 34.14, 30.24, 24.69, 33.03],
         [13.37, 17.79, 23.46, 22.09, 20.18, 20.49, 19.56],
@@ -23,12 +23,12 @@ DATA_LONGBENCH = {
 DATA_MMLU = {
     'row_headers': [
         'LLaMA2 7B', 
-        'StreamingLLM k512', 
-        'StreamingLLM k1024', 
-        'HiP k512 PP', 
-        'HiP k1024 PP',
+        'S.LLM k512', 
+        'S.LLM k1024', 
+        'HiP k512', 
+        'HiP k1024',
     ],
-    'col_headers': ['Humani-\nties', 'STEM', 'Social\nSciences', 'Other', 'Avg.'],
+    'col_headers': ['Humanities', 'STEM', 'Social Sciences', 'Other', 'Average'],
     'data': [
         [47.24, 34.81, 48.51, 44.59, 42.76],
         [45.74, 35.17, 45.76, 42.16, 41.40],
@@ -41,13 +41,13 @@ DATA_MMLU = {
 }
 
 DATA_LMEVAL = {
-    # 'row_headers': ['LLaMA2 7B', 'StreamingLLM k128', 'StreamingLLM k256', 'StreamingLLM k512', 'HiP k128 PP', 'HiP k256 PP', 'HiP k512 PP'],
+    # 'row_headers': ['LLaMA2 7B', 'S.LLM k128', 'S.LLM k256', 'S.LLM k512', 'HiP k128 PP', 'HiP k256 PP', 'HiP k512 PP'],
     'row_headers': [
         'LLaMA2 7B', 
-        'StreamingLLM k256', 
-        'StreamingLLM k512', 
-        'HiP k256 PP', 
-        'HiP k512 PP'
+        'S.LLM k256', 
+        'S.LLM k512', 
+        'HiP k256', 
+        'HiP k512'
     ],
     'col_headers': ['ARC', 'H.S.', 'MMLU', 'T.QA', 'Win.', 'GSM8k', 'Avg.'],
     'data': [
@@ -66,12 +66,12 @@ DATA_LMEVAL = {
 DATA_LMMSEVAL = {
     'row_headers': [
         'LLaVAv1.6 13B',
-        'StremaingLLM k512',
-        'StremaingLLM k1024',
-        'HiP k512 PP',
-        'HiP k1024 PP',
+        'S.LLM k512',
+        'S.LLM k1024',
+        'HiP k512',
+        'HiP k1024',
     ],
-    'col_headers': ['MME\ncog.', 'MME\npercep.', 'MMMU', 'D.VQA', 'GQA', 'T.VQA', 'Average'],
+    'col_headers': ['MME Cog.', 'MME Percep.', 'MMMU', 'DocVQA', 'GQA', 'TextVQA', 'Average'],
     'data': [
         [324.29, 1575.21, 0.359, 0.7738, 0.6536, 0.6692, 100.0],
         [242.14, 1093.55, 0.313, 0.2475, 0.5171, 0.2676, 63.73],
@@ -88,8 +88,8 @@ import matplotlib.pyplot as plt
 from timber.utils import setup_seaborn
 
 setup_seaborn(
-    legend_fontsize=6,
-    axis_below=False
+    legend_fontsize=5,
+    axis_below=True,
 )
 
 def loop_list(lst):
@@ -109,11 +109,12 @@ def render_rader(chart, name, title, root='saves/raders/'):
     
     label_loc = np.linspace(0, 2*np.pi, len(grades[0]))
     
-    plt.figure(figsize=(2.5, 2.5))
+    plt.figure(figsize=(2.4, 2.8))
     ax = plt.subplot(polar=True)
-    ax.set_title(title)
+    ax.set_title(title, pad=15)
     ax.set_theta_offset(chart['theta_offset'])
-    ax.tick_params(axis='x', which='major', pad=-25)
+    
+    ax.tick_params(axis='x', which='major', pad=-5)
     plt.xticks(label_loc, labels=categories)
     for idx, (grade, label) in enumerate(zip(grades, chart['row_headers'])):
         if idx == 0:
@@ -121,11 +122,30 @@ def render_rader(chart, name, title, root='saves/raders/'):
         else:
             linestyle = '-' if 'hip' in label.lower() else '-.'
             ax.plot(label_loc, grade, label=label, linewidth=2.0, linestyle=linestyle)
-    ax.legend(loc='lower center', bbox_to_anchor=(0.5, -0.25), ncols=2)
+    
+    labels = []
+    for label, angle in zip(ax.get_xticklabels()[1:], np.degrees([(i + 1) / len(chart['col_headers']) * 3.14 * 2 + chart['theta_offset'] for i in range(len(chart['col_headers']))])):
+        x,y = label.get_position()
+        lab = ax.text(
+            x, y, 
+            label.get_text(), 
+            transform=label.get_transform(), 
+            ha=label.get_ha(), 
+            va=label.get_va(), 
+            fontsize=6
+        )
+        angle = (angle % 360) + 90
+        if angle >= 90 and angle <= 270:
+            angle += 180
+        lab.set_rotation(angle)
+        labels.append(lab)
+    ax.set_xticklabels([])
+    
+    ax.legend(loc='lower center', bbox_to_anchor=(0.5, -0.25), ncols=3)
     ax.set_ylim([chart['ymin'], np.max(data) * 1.0])
     
-    plt.savefig(os.path.join(root, name + '.png'), dpi=200, bbox_inches="tight", pad_inches=0.01)
-    plt.savefig(os.path.join(root, name + '.pdf'), dpi=200, bbox_inches="tight", pad_inches=0.01)
+    plt.savefig(os.path.join(root, name + '.png'), dpi=200, pad_inches=0)
+    plt.savefig(os.path.join(root, name + '.pdf'), dpi=200, pad_inches=0)
     print('saved', os.path.join(root, name + '.png'))
 
 if __name__ == '__main__':
