803b6bb87d6d01f13f62c6c267727e3e1edf0081
AinL <gmlwns5176@gmail.com>
2024-07-14 18:39:38 +0900

    working well with low res

--- hip/models/attention.py
+++ hip/models/attention.py
@@ -279,22 +279,26 @@ def custom_attention(
                     self_extend_neighboor_window=1024,
                     self_extend_group_size=4,
                     
-                    topk_head_group_size=1,
+                    topk_head_group_size=4,
                     sample_method='first',
                     branch_method='half',
                     
                     # this may good or not, but definatly great with self-extend
-                    traverse_from_last_step=False,
-                    step_size=64,
-                    num_samples=1,
-                    chunk_size=None,
+                    traverse_from_last_step=True,
+                    step_size=1,
+                    num_samples=2,
+                    chunk_size=512,
                     # NOTE: this is significant when topk_head_group_size > 1. otherwise, this make worse result
-                    num_unions=1,
+                    num_unions=2,
                     
                     score_head_group_size=1,
                     
                     using_sparq=False,
                     sparq_hid=32,
+                    
+                    low_res_sample_scale=4,
+                    low_res_oversample_rate=2,
+                    low_res_oversample_block_stride_k=2,
                 )
         except RuntimeError as ex:
             os.makedirs('cache/hip', exist_ok=True)
