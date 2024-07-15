d8745cf746160900100e3209fab34adabf53c58e
AinL <gmlwns5176@gmail.com>
2024-07-15 17:32:03 +0900

    update attention norm

--- hip/models/hip_attention/attention_norm.py
+++ hip/models/hip_attention/attention_norm.py
@@ -17,21 +17,44 @@ def load_tokens(
         mask = mask_t[:, None]
     )
 
+@triton.autotune(
+    configs=[
+        triton.Config({'BLOCK_T': 128}, num_warps=2, num_stages=2),
+        triton.Config({'BLOCK_T': 128}, num_warps=4, num_stages=2),
+        triton.Config({'BLOCK_T': 128}, num_warps=8, num_stages=2),
+        
+        triton.Config({'BLOCK_T': 256}, num_warps=4, num_stages=2),
+        triton.Config({'BLOCK_T': 256}, num_warps=8, num_stages=2),
+        triton.Config({'BLOCK_T': 256}, num_warps=16, num_stages=2),
+        
+        triton.Config({'BLOCK_T': 64}, num_warps=1, num_stages=2),
+        triton.Config({'BLOCK_T': 64}, num_warps=2, num_stages=2),
+        triton.Config({'BLOCK_T': 64}, num_warps=4, num_stages=2),
+    ],
+    key=[
+        'HID',
+        'BLOCK_SIZE_Q', 
+        'BLOCK_SIZE_K',
+    ],
+    use_cuda_graph=True,
+)
 @triton.jit
 def attention_norm_cuda(
     Q, stride_q_n, stride_q_tdst, stride_q_hid,
     K, stride_k_n, stride_k_tsrc, stride_k_hid,
     
-    NORM, stride_norm_n, stride_norm_tdst,
+    NORM, stride_norm_n, stride_norm_bdst,
     
     TDST, TSRC,
     
     HID: tl.constexpr,
     BLOCK_SIZE_Q: tl.constexpr,
     BLOCK_SIZE_K: tl.constexpr,
+    
+    BLOCK_T: tl.constexpr,
 ):
-    idx_n = tl.program_id(0)
-    idx_bdst = tl.program_id(1)
+    idx_n = tl.program_id(1)
+    idx_bdst = tl.program_id(0)
     idx_tdst = tl.arange(0, BLOCK_SIZE_Q) + idx_bdst * BLOCK_SIZE_Q
     mask_tdst = idx_tdst < TDST
     
@@ -40,9 +63,9 @@ def attention_norm_cuda(
         idx_n, idx_tdst, mask_tdst, HID
     )
     
-    score_max = tl.full((BLOCK_SIZE_Q, ), dtype=tl.float32, value=float('-inf'))
-    for i_tsrc in range(0, TSRC, BLOCK_SIZE_K):
-        idx_tsrc = i_tsrc + tl.arange(0, BLOCK_SIZE_K)
+    score_max = tl.full((), dtype=tl.float32, value=float('-inf'))
+    for i_tsrc in range(0, TSRC, BLOCK_T):
+        idx_tsrc = i_tsrc + tl.arange(0, BLOCK_T)
         mask_tsrc = idx_tsrc < TSRC
         
         k = load_tokens(
@@ -62,12 +85,12 @@ def attention_norm_cuda(
         
         score_max = tl.maximum(
             score_max,
-            tl.max(qk, axis=-1)
+            tl.max(qk)
         )
     
-    exp_score_sum = tl.zeros((BLOCK_SIZE_Q, ), dtype=tl.float32)
-    for i_tsrc in range(0, TSRC, BLOCK_SIZE_K):
-        idx_tsrc = i_tsrc + tl.arange(0, BLOCK_SIZE_K)
+    exp_score_sum = tl.zeros((), dtype=tl.float32)
+    for i_tsrc in range(0, TSRC, BLOCK_T):
+        idx_tsrc = i_tsrc + tl.arange(0, BLOCK_T)
         mask_tsrc = idx_tsrc < TSRC
         
         k = load_tokens(
@@ -86,12 +109,15 @@ def attention_norm_cuda(
         )
         
         qk = qk - score_max[:, None]
+        qk = tl.reshape(qk, (BLOCK_SIZE_Q, BLOCK_T // BLOCK_SIZE_K, BLOCK_SIZE_K))
+        qk = tl.max(qk, axis=0)
+        qk = tl.max(qk, axis=-1)
         qk = tl.exp(qk)
-        exp_score_sum += tl.sum(qk, axis=-1)
+        exp_score_sum += tl.sum(qk)
     
-    norm_sum = tl.zeros((BLOCK_SIZE_Q, ), dtype=tl.float64)
-    for i_tsrc in range(0, TSRC, BLOCK_SIZE_K):
-        idx_tsrc = i_tsrc + tl.arange(0, BLOCK_SIZE_K)
+    norm_sum = tl.zeros((), dtype=tl.float64)
+    for i_tsrc in range(0, TSRC, BLOCK_T):
+        idx_tsrc = i_tsrc + tl.arange(0, BLOCK_T)
         mask_tsrc = idx_tsrc < TSRC
         
         k = load_tokens(
@@ -110,6 +136,9 @@ def attention_norm_cuda(
         )
         
         qk = qk - score_max[:, None]
+        qk = tl.reshape(qk, BLOCK_SIZE_Q, BLOCK_T // BLOCK_SIZE_K, BLOCK_SIZE_K)
+        qk = tl.max(qk, axis=0)
+        qk = tl.max(qk, axis=-1)
         prob = tl.exp(qk) / tl.maximum(exp_score_sum[:, None], 1e-20)
         norm_sum += tl.sum(prob * prob, axis=-1)
     
@@ -118,14 +147,16 @@ def attention_norm_cuda(
     tl.store(
         NORM +\
             idx_n * stride_norm_n +\
-            idx_tdst * stride_norm_tdst,
+            idx_bdst[:] * stride_norm_bdst,
         value=norm,
-        mask=mask_tdst,
     )
 
 def attention_norm(
     q: Tensor,
     k: Tensor,
+    
+    BLOCK_SIZE_Q: int = 32,
+    BLOCK_SIZE_K: int = 2,
 ):
     """
     q: fp*[N, TDST, HID]
@@ -136,16 +167,18 @@ def attention_norm(
     """
     assert q.ndim == 3
     assert q.shape == k.shape
+    assert q.device == k.device
     
     N, TDST, HID = q.shape
     _, TSRC, _ = k.shape
     
-    norm = torch.zeros((N, TDST), dtype=torch.float32, device=q.device)
+    norm = torch.zeros(
+        (N, triton.cdiv(TDST, BLOCK_SIZE_Q)), 
+        dtype=torch.float32, 
+        device=q.device
+    )
     
-    BLOCK_SIZE_Q = 32
-    BLOCK_SIZE_K = 64
-    
-    grid = (N, triton.cdiv(TDST, BLOCK_SIZE_Q))
+    grid = (triton.cdiv(TDST, BLOCK_SIZE_Q), N)
     
     pre_device = torch.get_default_device()
     torch.set_default_device(q.device)
@@ -159,9 +192,6 @@ def attention_norm(
         q.shape[-1],
         BLOCK_SIZE_Q, 
         BLOCK_SIZE_K,
-        
-        num_warps=4,
-        num_stages=2,
     )
     torch.set_default_device(pre_device)
     
@@ -170,8 +200,13 @@ def attention_norm(
 if __name__ == '__main__':
     q = torch.randn((32, 8192, 128), dtype=torch.float16, device=0)
     k = torch.randn((32, 8192, 128), dtype=torch.float16, device=0)
+    block_size_q = 32
+    block_size_k = 2
     
-    for _ in range(10):
+    num_samples = 50
+    
+    elapsed_truth = 0
+    for i in range(num_samples):
         torch.cuda.synchronize()
         t = time.time()
         truth = q @ k.transpose(-1, -2)
@@ -180,17 +215,33 @@ if __name__ == '__main__':
             truth,
             float('-inf')
         )
+        truth = truth.view(
+            q.shape[0], 
+            q.shape[1] // block_size_q, 
+            block_size_q, 
+            k.shape[1] // block_size_k, 
+            block_size_k
+        )
+        truth = truth.amax(-1).amax(-2)
         truth = torch.norm(torch.softmax(truth, dim=-1), dim=-1)
         torch.cuda.synchronize()
-        elapsed_turth = time.time() - t
-    print(truth[-1])
+        if i > 3:
+            elapsed_truth += time.time() - t
     
-    for _ in range(10):
+    elapsed_triton = 0
+    for i in range(num_samples):
         torch.cuda.synchronize()
         t = time.time()
         norm = attention_norm(q, k)
         torch.cuda.synchronize()
-        elapsed_triton = time.time() - t
-    print(norm[-1])
+        if i > 3:
+            elapsed_triton += time.time() - t
     
-    print(elapsed_turth * 1000, elapsed_triton * 1000)
+    print('torch', truth[-1, -10:].tolist())
+    print('triton', norm[-1, -10:].tolist())
+    
+    elapsed_truth /= (num_samples - 4)
+    elapsed_triton /= (num_samples - 4)
+    
+    print('torch', elapsed_truth * 1000)
+    print('triton', elapsed_triton * 1000)
