7cb797882ccd61fcf1ba5bf8de8aa50934c120b8
Heejun Lee <gmlwns5176@gmail.com>
2024-09-11 03:00:39 +0000

    dual stream test

--- hip/models/hip_attention/scripts/bench_dual_stream_masking.py
+++ hip/models/hip_attention/scripts/bench_dual_stream_masking.py
@@ -1,10 +1,14 @@
 import torch
+import tqdm
 from hip import hip_attention_11
 from hip.models.hip_attention.attention1_block_gpu import load_checkouts, to_dense
+import time
 
 def main():
     refresh_interval = 8
-    samples = 100
+    samples = 500
+    batch_size = 32
+    sequential_masking = False
     
     q, k, v, out, cos, sin = load_checkouts(
         idx=0, 
@@ -31,15 +35,36 @@ def main():
     v = reshape(v, HEAD_KV)
     out = reshape(out, HEAD)
     
+    q = q[:, -1:, :, :]
+    q = q.expand(batch_size, -1, -1, -1)
+    k = k.expand(batch_size, -1, -1, -1)
+    v = v.expand(batch_size, -1, -1, -1)
+    
     sa_stream = torch.cuda.Stream()
     mask_stream = torch.cuda.Stream()
     
     _, meta = hip_attention_11(q, k, v)
     
-    for i in range(samples):
+    t_start = time.time()
+    
+    for i in tqdm.tqdm(range(samples), dynamic_ncols=True):
+        if i == 3:
+            t_start = time.time()
         with torch.cuda.stream(mask_stream):
-            _, new_meta = hip_attention_11(q, k, v)
+            _, new_meta = hip_attention_11(q, k, v, mask_only=True)
         
+        for isa in range(refresh_interval):
+            with torch.cuda.stream(sa_stream if not sequential_masking else mask_stream):
+                context, _ = hip_attention_11(q, k, v, previous_metadata=meta)
+        
+        torch.cuda.default_stream().wait_stream(mask_stream)
+        torch.cuda.default_stream().wait_stream(sa_stream)
+        torch.cuda.synchronize()
+    
+    t_end = time.time()
+    
+    elapsed = t_end - t_start
+    print(elapsed * 1000)
 
 if __name__ == '__main__':
     main()
