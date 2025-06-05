512dce4b0f16332edb9dbd6979980382f4d10e6a
AinL <gmlwns5176@gmail.com>
2025-06-05 08:15:30 +0000

    fix

--- src/hip_attn/v1_2/attention_extend.py
+++ src/hip_attn/v1_2/attention_extend.py
@@ -32,6 +32,7 @@ from hip_attn.v1_2.compute_v_cos import compute_v_cos
 from hip_attn.v1_2.eval_stage import calculate_chunk_score
 from hip_attn.v1_2.scan_stage import chunk_controllable_sampling_mask_cuda
 from hip_attn.v1_2.landmark_sample import landmark_sample
+from hip_attn.v1_2.stage_prologue import stage_prologue
 
 try:
     import torch.distributed as dist
@@ -362,99 +363,31 @@ def dual_stage_quadratic_hip_attention(
                 # print('last cached stage', i_stage)
                 pass
             elif i_stage > 0:
-                assert (stage_k % chunk_size) == 0, f"{stage_k} % {chunk_size}"
-                indices_left = indices_left[..., : stage_k // chunk_size]
-                require_align = stage_info.require_realign_index
-                if require_align:
-                    indices_left = (
-                        indices_left - args.sink_token_size
-                    ) // chunk_size * chunk_size + args.sink_token_size
-                    indices_right = indices_left + chunk_size
-                else:
-                    indices_right = indices_right[..., : stage_k // chunk_size]
-                out_scores = out_scores[..., : stage_k // chunk_size]
-                # NOTE: revert this
-                if stage_info.require_reset_score:
-                    out_scores.fill_(-32000.0)
-
-                indices_left, t_indices = indices_left.sort(dim=-1)
-                indices_right = indices_right.gather(dim=-1, index=t_indices)
-                out_scores = out_scores.gather(dim=-1, index=t_indices)
-
-                if BLOCK_SIZE_Q != stage_info.stage_block_size_q:
-                    assert stage_info.stage_block_size_q > 0
-                    assert BLOCK_SIZE_Q > stage_info.stage_block_size_q
-                    assert (BLOCK_SIZE_Q % stage_info.stage_block_size_q) == 0
-
-                    num_split = BLOCK_SIZE_Q // stage_info.stage_block_size_q
-                    BLOCK_SIZE_Q = stage_info.stage_block_size_q
-                    BDST = triton.cdiv(TDST, BLOCK_SIZE_Q)
-                    BDST_SCAN = triton.cdiv(BDST, STAGE_STRIDE)
-
-                    indices_left = indices_left.repeat_interleave(num_split, 1)[
-                        :, -BDST:
-                    ].contiguous()
-                    indices_right = indices_right.repeat_interleave(num_split, 1)[
-                        :, -BDST:
-                    ].contiguous()
-                    out_scores = out_scores.repeat_interleave(num_split, 1)[
-                        :, -BDST:
-                    ].contiguous()
-
-                if STAGE_STRIDE != stage_info.stage_stride:
-                    assert stage_info.stage_stride < STAGE_STRIDE
-                    assert STAGE_STRIDE > 0
-                    indices_left = indices_left.repeat_interleave(
-                        STAGE_STRIDE // stage_info.stage_stride, 1
-                    )[:, -BDST:].contiguous()
-                    indices_right = indices_right.repeat_interleave(
-                        STAGE_STRIDE // stage_info.stage_stride, 1
-                    )[:, -BDST:].contiguous()
-                    out_scores = out_scores.repeat_interleave(
-                        STAGE_STRIDE // stage_info.stage_stride, 1
-                    )[:, -BDST:].contiguous()
-                    STAGE_STRIDE = stage_info.stage_stride
-
-                # if DEBUG and DEBUG_RENDER and (not torch.cuda.is_current_stream_capturing()) and (BDST > 10) and (i_stage == 1):
-                #     out_indices_cpu = indices_left.cpu().numpy()
-                #     debug = np.zeros((triton.cdiv(TDST, BLOCK_SIZE_Q), triton.cdiv(TSRC, BLOCK_CHUNK)))
-                #     render_plot_sampled(out_indices_cpu, debug, DEBUG_HEAD, BLOCK_CHUNK, chunk_count, TDST, args.sink_token_size)
-                #     cv2.imwrite('dummy_sampled.png', debug * 255)
-                #     print('saved dummy_sampled.png')
-
-                assert (chunk_size % stage_chunk_size) == 0
-                splits = chunk_size // stage_chunk_size
-                chunk_sizes = (
-                    (indices_right - indices_left).float() / splits
-                ).clamp_min_(0)
-                indices_left = (
-                    indices_left[..., None]
-                    + (
-                        torch.arange(0, splits, device=q.device)[
-                            None, None, None, None, :
-                        ]
-                        * chunk_sizes[..., None]
-                    )
-                    .floor()
-                    .long()
+                (
+                    indices_left, 
+                    indices_right, 
+                    out_scores, 
+                    BLOCK_SIZE_Q, 
+                    BDST, 
+                    STAGE_STRIDE
+                ) = stage_prologue(
+                    q, 
+                    indices_left,
+                    indices_right,
+                    out_scores,
+                    
+                    stage_k, 
+                    stage_chunk_size, 
+                    chunk_size, 
+                    stage_info, 
+                    
+                    args, 
+                    
+                    TDST,
+                    BDST,
+                    STAGE_STRIDE,
+                    BLOCK_SIZE_Q,
                 )
-                indices_left = indices_left.flatten(-2, -1)
-                indices_right = (
-                    indices_right[..., None]
-                    - (
-                        (
-                            (splits - 1)
-                            - torch.arange(0, splits, device=q.device)[
-                                None, None, None, None, :
-                            ]
-                        )
-                        * chunk_sizes[..., None]
-                    )
-                    .floor()
-                    .long()
-                )
-                indices_right = indices_right.flatten(-2, -1)
-                out_scores = out_scores.repeat_interleave(splits, -1)
             else:
                 assert stage_info.stage_k is None, "first stage always quadratic"
                 assert isinstance(
@@ -939,7 +872,6 @@ def dual_stage_quadratic_hip_attention(
                         )
 
                     out_scores = torch.amax(out_scores, keepdim=True, dim=2)
-
                     out_scores = torch.broadcast_to(out_scores, ori_shape).contiguous()
                 else:
                     args.disable_flashdecode = True
