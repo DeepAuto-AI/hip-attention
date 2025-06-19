279eaaf8a9dbc8e5d87dccbe16fd887a894ed1fe
Bumsik Kim <k.bumsik@gmail.com>
2025-06-19 00:09:44 +0000

    handle n

--- src/hip_attn/v1_2/paged_hip.py
+++ src/hip_attn/v1_2/paged_hip.py
@@ -1,3 +1,4 @@
+import copy
 import os
 import warnings
 from typing import Any, Optional
@@ -143,6 +144,14 @@ def forward_paged_hip(
 
         if cached_metadata is not None:
             states = cached_metadata.state
+            if isinstance(states, list) and (len(states) < len(extend_seq_lens_cpu)):
+                assert (len(extend_seq_lens_cpu) % len(states)) == 0
+                n_repeat = len(extend_seq_lens_cpu) // len(states)
+                new_states = []
+                for state in states:
+                    for _ in range(n_repeat):
+                        new_states.append(copy.deepcopy(state))
+                states = new_states
 
         start_len = 0
         decoding_reqs = []
